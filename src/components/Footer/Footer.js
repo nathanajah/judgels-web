2ae09843fb9bbb923a00112bf8bf7039c965deb1
Nathan Azaria <nathanajah@yahoo.com>
2017-02-03 16:30:31 +0700

    Added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.scss'
+
+export const Footer = () => (
+  <footer>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-12'>
+          <div className='pull-left first'>
+            <div className='copyright'>
+              &#169; 2017 TLX
+            </div>
+          </div>
+          <div className='pull-right first'>
+            Powered by Judgels Jophiel 0.8.0. Sponsored by SIRCLO .
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
