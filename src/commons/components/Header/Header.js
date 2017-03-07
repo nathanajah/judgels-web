67b31132411f3e167bca66ad71638b068fee5914
Irvan Putra <irvan.putra@gmail.com>
2017-03-08 03:54:15 +0700

    Modify header so be like new-design

--- src/commons/components/Header/Header.js
+++ src/commons/components/Header/Header.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import './Header.scss'
 import Logo from 'commons/assets/images/logo.png'
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+import faker from 'faker'
+
+const trigger = (
+  <span>
+    <Image avatar src={faker.internet.avatar()} />&nbsp;
+    <span>{faker.name.findName()}</span><br />
+    <small> Username: {faker.internet.userName()}</small>
+  </span>
+)
 
 export const Header = () => (
-  <header>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-12'>
-          <div className='pull-left'>
-            <img className='logo' src={Logo} alt='Logo' />
-          </div>
-          <div className='title pull-left'>
-            <h1>Single Sign-On</h1>
-          </div>
-          <nav className='right-info pull-right'>
-            <div className='languages'>
-              <select id='languages' className='form-control selectpicker'>
-                <option value='en-US'>EN-US</option>
-                <option value='en-GB'>en-GB</option>
-              </select>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+  <Menu inverted color='blue'>
+    <Menu.Item><Image size='mini' src={Logo} /></Menu.Item>
+    <Menu.Item header><span>TLX | TOKI Learning Center</span></Menu.Item>
+    <Menu.Menu position='right'>
+      <Dropdown trigger={trigger} item>
+        <Dropdown.Menu>
+          <Dropdown.Item>Profile</Dropdown.Item>
+          <Dropdown.Item>Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu.Menu>
+  </Menu>
 )
 
 export default Header
