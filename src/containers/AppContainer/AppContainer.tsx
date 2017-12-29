0209cb738a893269ae45c53a4f8ecd75b31f6cdc
Ashar Fuadi <fushar@gmail.com>
2017-12-29 13:57:54 +0700

    Add footer

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 import HeaderContainer from '../HeaderContainer/HeaderContainer';
 import JophielContainer from '../../routes/Jophiel/containers/JophielContainer/JophielContainer';
+import { Footer } from '../../components/Footer/Footer';
 
 const AppContainer = () => (
   <div>
     <HeaderContainer />
     <JophielContainer />
+    <Footer />
   </div>
 );
 
