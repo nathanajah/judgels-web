ad851d9f4512e4b8e0dd64bd4f3ff9b01f8d8152
Ashar Fuadi <fushar@gmail.com>
2018-01-12 21:18:01 +0700

    Redesign temp homepage

--- src/routes/jophiel/home/Home/Home.tsx
+++ src/routes/jophiel/home/Home/Home.tsx
@@ -1,28 +1,31 @@
 import { Card, Icon } from '@blueprintjs/core';
 import * as React from 'react';
 
-import { FullPageLayout } from '../../../../components/layouts/FullPageLayout/FullPageLayout';
+import { SingleColumnLayout } from '../../../../components/layouts/SingleColumnLayout/SingleColumnLayout';
+
+import './Home.css';
 
 const redirectToUriel = () => (location.href = (window as any).env.URIEL_URL);
 const redirectToJerahmeel = () => (location.href = (window as any).env.JERAHMEEL_URL);
 
 export const Home = () => (
-  <FullPageLayout>
-    <Card>
-      <h5>
-        <Icon iconName="clipboard" />&nbsp;&nbsp;&nbsp;Apologies! The shiny new {(window as any).env.APP_NAME} site is
-        under construction.&nbsp;&nbsp;&nbsp;<Icon iconName="automatic-updates" />
-      </h5>
-      <hr />
-      <p>Until then, you can access the legacy modules below:</p>
-      <br />
-      <Card interactive onClick={redirectToUriel}>
-        Competition Gate
-      </Card>
-      <br />
-      <Card interactive onClick={redirectToJerahmeel}>
-        Training Gate
-      </Card>
+  <SingleColumnLayout>
+    <h5 className="home__text">
+      <Icon iconName="oil-field" className="home__icon" />
+    </h5>
+    <h5 className="home__text">Apologies! The new {(window as any).env.APP_NAME} site is still under construction.</h5>
+    <p className="home__text home__text--small">Until then, you can access the legacy modules below:</p>
+    <br />
+    <Card interactive onClick={redirectToUriel}>
+      <p className="home__link">
+        <Icon iconName="walk" /> Competition Gate
+      </p>
     </Card>
-  </FullPageLayout>
+    <br />
+    <Card interactive onClick={redirectToJerahmeel}>
+      <p className="home__link">
+        <Icon iconName="pulse" /> Training Gate
+      </p>
+    </Card>
+  </SingleColumnLayout>
 );
