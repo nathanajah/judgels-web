6a7791da018ea18fd31bb1b8cd174b10bc9e3ef2
Nathan Azaria <nathanajah@yahoo.com>
2018-02-03 12:37:42 +0000

    Add menu to menubar

--- src/components/Menubar/Menubar.tsx
+++ src/components/Menubar/Menubar.tsx
@@ -1,14 +1,61 @@
 import { Tab2, Tabs2 } from '@blueprintjs/core';
+import { withRouter, NavLink, matchPath } from 'react-router-dom';
 import * as React from 'react';
+import { History, Location } from 'history';
+import { RouteComponentProps } from 'react-router';
 
 import './Menubar.css';
 
-export const Menubar = () => (
-  <div className="menubar">
-    <div className="menubar__content">
-      <Tabs2 id="menubar" renderActiveTabPanelOnly>
-        <Tab2 id="home">Home</Tab2>
-      </Tabs2>
-    </div>
-  </div>
-);
+const tabToPath = (id: string) => {
+  switch(id) {
+    case 'home':
+      return '/';
+    case 'competition':
+      return '/competition';
+    default:
+      return '/';
+  }
+}
+
+const navigateTo =  (history: History, id: string) => {
+  const path = tabToPath(id);
+  history.push({
+    pathname: path
+  });
+}
+
+const pathToTab = (location: Location) => {
+  if (matchPath(location.pathname, {path: '/', exact: true})) {
+    return 'home';
+  } else if (matchPath(location.pathname, {path: '/competition'})) {
+    return 'competition';
+  } else {
+    return 'home';
+  }
+}
+
+class MenubarComponent extends React.Component<RouteComponentProps<{}>> {
+  render() {
+    const selectedTabId = pathToTab(this.props.location);
+
+    return  (
+      <div className="menubar">
+        <div className="menubar__content">
+          <Tabs2
+            id="menubar"
+            renderActiveTabPanelOnly
+            selectedTabId={selectedTabId}
+            onChange={navigateTo.bind(null, this.props.history)}
+          >
+            <Tab2 id="home">Home</Tab2>
+            <Tab2 id="competition">
+              <NavLink to="/competition"> Competition </NavLink>
+            </Tab2>
+          </Tabs2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export const Menubar = withRouter<{}>(MenubarComponent);
